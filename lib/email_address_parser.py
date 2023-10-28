fe36859d4bc421b0152e4a7c69ee72dd33f8fc46
Alpha <alpha.likembe@student.moringaschool.com>
2023-10-28 04:27:50 +0300

    trials

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        email_addresses = re.findall(email_pattern, self.email_string)
+        email_addresses.sort()
+
+        return email_addresses
+    
+
+# Example usage:
+email_string = "Please contact support@example.com or sales@company.com for assistance."
+parser = EmailAddressParser(email_string)
+parsed_emails = parser.parse()
+print(parsed_emails)
